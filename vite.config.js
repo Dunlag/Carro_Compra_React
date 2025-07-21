e5f7fd3e060410890df08e60805748db5ddeb8ce
Fernando <fernando.pinilla85@gmail.com>
2025-07-21 18:50:04 +0200

    Add gh-pages deployment script and update Vite configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Configuración de Vite para publicar en GitHub Pages
+// La propiedad base indica la subcarpeta donde estará el proyecto
 export default defineConfig({
+  base: '/Carro_Compra_React/',
   plugins: [react()],
 })
